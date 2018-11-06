2c92c2bc1c47cc147db80306e7eca044ec6af19f
Vinh Luu <vinhlt3@gmail.com>
2018-11-06 16:20:18 +0700

    Set focus on Qty input when render line-item-row

--- app/components/rows/line-item-row/component.js
+++ app/components/rows/line-item-row/component.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didInsertElement() {
+    let firstInput = this.get('element').querySelector("input");
+    firstInput.focus();
+    firstInput.select();
+  }
 });
